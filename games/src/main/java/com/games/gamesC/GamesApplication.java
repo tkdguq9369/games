a199351453bab4d90c6b60cdd8192f49875d9394
kim <kim@61.81.148.96>
2020-05-18 04:51:56 +0900

    layout 수정

--- games/src/main/java/com/games/gamesC/GamesApplication.java
+++ games/src/main/java/com/games/gamesC/GamesApplication.java
@@ -2,6 +2,8 @@ package com.games.gamesC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class GamesApplication {
@@ -9,5 +11,6 @@ public class GamesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamesApplication.class, args);
 	}
+
 
 }
