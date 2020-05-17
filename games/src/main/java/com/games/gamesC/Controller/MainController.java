2891386cb6623c876cc71521d6dba2be7e40421a
kim <kim@61.81.148.96>
2020-05-18 03:21:42 +0900

    게임 모음집 1일차 프로젝트 생성

---
+++ games/src/main/java/com/games/gamesC/Controller/MainController.java
@@ -0,0 +1,13 @@
+package com.games.gamesC.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
---
+++ games/src/test/java/com/games/gamesC/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.gamesC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
